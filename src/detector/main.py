5af6509d448af48afd407a01dd103f29ef95d258
RozanovN <rozanovny@gmail.com>
2023-11-22 09:25:53 -0800

    Add alexnet model for image detection skeleton

--- src/detector/main.py
+++ src/detector/main.py
@@ -1,2 +1,30 @@
+from keras.src.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+from tensorflow.keras import Input
+from tensorflow.keras.models import Sequential
+
+def alexnet():
+    model = Sequential()
+    model.add(Input((224, 224, 3)))
+    model.add(Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), padding="valid", activation='relu'))
+    model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="valid"))
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(filters=384, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="valid"))
+    model.add(Flatten())
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dense(units=3, activation="softmax"))
+
+    model.compile(
+        optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    return model
+
 def main():
   pass
