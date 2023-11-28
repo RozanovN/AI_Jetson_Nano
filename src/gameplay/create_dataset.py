78e98bc35c00159ee30a23704bdbbac1c8a4692d
CY-Jimmy <51365909+cyjimmy@users.noreply.github.com>
2023-11-28 01:16:33 -0800

    Filter input files when creating dataset

--- src/gameplay/create_dataset.py
+++ src/gameplay/create_dataset.py
@@ -9,9 +9,12 @@ output_dir_path = os.path.join(os.path.dirname(__file__), "..", "..", "datasets"
 pattern =  re.compile(r'\b\d+,\d+,\d+\b')
 size_of_board = 20
 initial_board_state = np.zeros((size_of_board, size_of_board), dtype=int)
+raw_data_file_extension = ".psq"
 
 # loop through all files in raw_data_dir_path
 for filename in os.listdir(raw_data_dir_path):
+    if not filename.endswith(raw_data_file_extension):
+        continue
     board_state = np.copy(initial_board_state)
     dataset = []
     with open(os.path.join(raw_data_dir_path, filename), "r") as f:
