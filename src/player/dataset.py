83f44484d6e3308d64cee52fdf1a10a3720fd429
Kirin Tang <ktang91@my.bcit.ca>
2023-11-27 19:43:27 -0800

    process gomocup data

--- src/player/dataset.py
+++ src/player/dataset.py
@@ -1,48 +1,43 @@
-import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
+import glob, os, re
 
-board_size = 15
+
+board_size = 20
+directory = 'datasets/gomocup/freestyle2_2022'
 
 
-def parse_xml_to_npy(xml_file):
-  tree = ET.parse(xml_file)
-  root = tree.getroot() 
-  games = root.find('games')
+def parse_gomocup_dataset():
   games_data = []
   
-  for game in games.findall('game'):
-    move_text = game.find('move').text
-    
-    if not move_text:
-      continue
-    
-    moves = move_text.split(' ')
-    game_id = game.get('id')
-    bresult = game.get('bresult')
-    rule = game.get('rule')
-    
-    if rule != '7':
-      continue
-    
-    board_states = []
-    board = np.zeros((board_size, board_size), dtype=np.int8)
-    
-    for turn, move in enumerate(moves):
-      row = ord(move[0]) - ord('a')
-      col = int(move[1:]) - 1
-      board[row][col] = turn % 2 + 1 # 1: black, 2: white
-      board_states.append(np.copy(board))
+  for filepath in glob.glob(os.path.join(directory, '*.psq')):
+    with open(filepath, 'r') as file:
+      filename = os.path.basename(filepath).split('.')[0]
+      id, result = filename.rsplit('_', 1)
+      board_states = []
+      board = np.zeros((board_size, board_size), dtype=np.int8)
       
-    games_data.append({
-      'id': game_id,
-      'moves': board_states,
-      'bresult': bresult,
-      'rule': rule,
-    })
-    
-  np.save('gomoku_pro_dataset.npy', games_data)
+      for idx, line in enumerate(file.readlines()[1:]):
+        if re.match(r'^\d+,\d+,\d+$', line.strip()):
+          col, row = line.strip().split(',')[:2]
+          board[int(row)-1][int(col)-1] = idx % 2 + 1 # 1: black, 2: white
+          board_states.append(np.copy(board))
+        else:
+          break
       
-      
-    
+      games_data.append({
+        'id': id,
+        'moves': board_states,
+        'result': result,
+      })  
+  
+  np.save('datasets/gomocup_2022_dataset.npy', games_data) 
+
+
+def main():
+  parse_gomocup_dataset()
+
+if __name__ == '__main__':
+  main()
+
     
--- src/player/main.py
+++ src/player/main.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-from dataset import parse_xml_to_npy
-
 def main():
-  # parse_xml_to_npy('datasets/renju_net/renjunet_v10_20231122.xml')
-  data = np.load('datasets/renju_net/gomoku_pro_dataset.npy', allow_pickle=True)
+  data = np.load('datasets/gomocup_2022_dataset.npy', allow_pickle=True)
 
 if __name__ == '__main__':
   main()
