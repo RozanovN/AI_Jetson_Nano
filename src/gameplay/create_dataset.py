f2d486ceae0b61e9505a95fe844f34b1d1e6ca90
CY-Jimmy <51365909+cyjimmy@users.noreply.github.com>
2023-11-28 19:45:32 -0800

    modify dataset script

--- src/gameplay/create_dataset.py
+++ src/gameplay/create_dataset.py
@@ -10,11 +10,15 @@ pattern =  re.compile(r'\b\d+,\d+,\d+\b')
 size_of_board = 20
 initial_board_state = np.zeros((size_of_board, size_of_board), dtype=int)
 raw_data_file_extension = ".psq"
+file_count = 1000
 
 # loop through all files in raw_data_dir_path
 for filename in os.listdir(raw_data_dir_path):
     if not filename.endswith(raw_data_file_extension):
         continue
+    if file_count == 0:
+        break
+    file_count -= 1
     board_state = np.copy(initial_board_state)
     dataset = []
     with open(os.path.join(raw_data_dir_path, filename), "r") as f:
@@ -26,10 +30,18 @@ for filename in os.listdir(raw_data_dir_path):
                 
                 board_state = -board_state
                 input = np.copy(board_state)
-                board_state[move_row][move_col] = 1
                 
                 output = np.copy(initial_board_state)
                 output[move_row][move_col] = 1
                 
-                dataset.append({'input': input, 'output': output})
+                flipped_input = np.flipud(input)
+                flipped_output = np.flipud(output)
+                
+                for input, output in [(input, output), (flipped_input, flipped_output)]:
+                    for i in range(4):
+                        rotated_input = np.rot90(input, i)
+                        rotated_output = np.rot90(output, i)
+                        dataset.append({'input': rotated_input, 'output': rotated_output})
+                
+                board_state[move_row][move_col] = 1
     np.savez(os.path.join(output_dir_path, filename.split(".")[0] + ".npz"), dataset=dataset)
