0ecd7d771dafb2bc1208c498f40885c4cde931ea
CY-Jimmy <51365909+cyjimmy@users.noreply.github.com>
2023-11-29 00:02:43 -0800

    change model name

--- src/gameplay/train.py
+++ src/gameplay/train.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
 from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras import layers
-
+from tensorflow.python.client import device_lib
 
 # Define the CNN model
 def create_cnn_model(input_shape):
@@ -23,7 +24,7 @@ input_shape = (20, 20, 1)
 
 # Load the dataset from the .npz file
 dataset_dir_path = os.path.join(os.path.dirname(__file__), "..", "..", "datasets", "gameplay", "processed")
-dataset_path = os.path.join(dataset_dir_path, "0_0_10_1.npz")
+dataset_path = os.path.join(dataset_dir_path, "dataset.npz")
 
 loaded_data = np.load(dataset_path, allow_pickle=True)
 dataset = loaded_data['dataset']
@@ -36,29 +37,7 @@ outputs = np.array([data['output'].flatten() for data in dataset])
 model = create_cnn_model(input_shape)
 
 # Train the model
-model.fit(inputs, outputs, epochs=10, batch_size=20)
+model.fit(inputs, outputs, epochs=10, batch_size=400)
 
 # Save the model
-model.save("test.keras")
-
-test_input = np.zeros((20, 20))
-# test_input[3][3] = 1
-# test_input[3][4] = 1
-# test_input[3][5] = 1
-# test_input[4][4] = -1
-# test_input[5][4] = -1
-# test_input[6][4] = -1
-# test_input[7][4] = -1
-test_input[2][2] = -1
-
-# Make predictions with the trained model
-predictions = model.predict(np.array([test_input]))
-
-# Convert predictions to row and column indices
-predicted_indices = np.argmax(predictions, axis=1)
-predicted_rows = predicted_indices // 20
-predicted_cols = predicted_indices % 20
-
-# Print the predicted rows and columns
-print("Predicted Rows:", predicted_rows)
-print("Predicted Columns:", predicted_cols)
+model.save("my_model.keras")
