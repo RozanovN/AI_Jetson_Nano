dc356d770805bf0b30dbfbd982aa01e9ff2cfa45
CY-Jimmy <51365909+cyjimmy@users.noreply.github.com>
2023-11-28 18:11:10 -0800

    format file

--- src/gameplay/ui.py
+++ src/gameplay/ui.py
@@ -2,70 +2,115 @@ import tkinter as tk
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model('20201213_202430.h5')
+my_model = False
+if my_model:
+    model = load_model("test.h5")
+else:
+    model = load_model("20201213_202430.h5")
+
 
 def on_click(event):
     # Do nothing if the game is over
     if game_over:
         return
-    
+
     col = round((event.x - board_left) / cell_size)
     row = round((event.y - board_top) / cell_size)
 
     # Draw the piece
     if 0 <= col < board_size and 0 <= row < board_size:
-        canvas.create_oval(col * cell_size + board_left - oval_offset, row * cell_size + board_top - oval_offset,
-                            (col + 1) * cell_size + board_left - oval_offset, (row + 1) * cell_size + board_top - oval_offset, fill="black")
-    
+        canvas.create_oval(
+            col * cell_size + board_left - oval_offset,
+            row * cell_size + board_top - oval_offset,
+            (col + 1) * cell_size + board_left - oval_offset,
+            (row + 1) * cell_size + board_top - oval_offset,
+            fill="black",
+        )
+
     # Update the board state
     board[row][col] = 1
-    
+
     # Check if the game is over
     check_game_over(player=1)
-    
-    # TODO: Make a prediction with the trained model
-    input = np.expand_dims(board, axis=(0, -1)).astype(np.float32)
-    output = model.predict(input).squeeze()
-    output = output.reshape((20, 20))
-    row, col = np.unravel_index(np.argmax(output), output.shape)
-    
+
+    # Make a prediction with the trained model
+    if my_model:
+        # TODO: Make a prediction with my model
+        pass
+    else:
+        input = np.expand_dims(board, axis=(0, -1)).astype(np.float32)
+        output = model.predict(input).squeeze()
+        output = output.reshape((20, 20))
+        row, col = np.unravel_index(np.argmax(output), output.shape)
+
     # Draw the predicted piece
-    canvas.create_oval(col * cell_size + board_left - oval_offset, row * cell_size + board_top - oval_offset,
-                            (col + 1) * cell_size + board_left - oval_offset, (row + 1) * cell_size + board_top - oval_offset, fill="white")
-    
+    canvas.create_oval(
+        col * cell_size + board_left - oval_offset,
+        row * cell_size + board_top - oval_offset,
+        (col + 1) * cell_size + board_left - oval_offset,
+        (row + 1) * cell_size + board_top - oval_offset,
+        fill="white",
+    )
+
     # Update the board state
     board[row][col] = -1
-    
+
     # Check if the game is over
     check_game_over(player=-1)
-    
+
+
 def check_game_over(player):
     global game_over
     won_player = 0
-    
+
     # Check if the player won
     for row in range(board_size):
         for col in range(board_size):
             try:
-                if board[row][col] == player and board[row+1][col] == player and board[row+2][col] == player and board[row+3][col] == player and board[row+4][col] == player:
+                if (
+                    board[row][col] == player
+                    and board[row + 1][col] == player
+                    and board[row + 2][col] == player
+                    and board[row + 3][col] == player
+                    and board[row + 4][col] == player
+                ):
                     won_player = player
                     break
             except:
                 pass
             try:
-                if board[row][col] == player and board[row][col+1] == player and board[row][col+2] == player and board[row][col+3] == player and board[row][col+4] == player:
+                if (
+                    board[row][col] == player
+                    and board[row][col + 1] == player
+                    and board[row][col + 2] == player
+                    and board[row][col + 3] == player
+                    and board[row][col + 4] == player
+                ):
                     won_player = player
                     break
             except:
                 pass
             try:
-                if board[row][col] == player and board[row+1][col+1] == player and board[row+2][col+2] == player and board[row+3][col+3] == player and board[row+4][col+4] == player:
+                if (
+                    board[row][col] == player
+                    and board[row + 1][col + 1] == player
+                    and board[row + 2][col + 2] == player
+                    and board[row + 3][col + 3] == player
+                    and board[row + 4][col + 4] == player
+                ):
                     won_player = player
                     break
             except:
                 pass
             try:
-                if col >= 4 and board[row][col] == player and board[row+1][col-1] == player and board[row+2][col-2] == player and board[row+3][col-3] == player and board[row+4][col-4] == player:
+                if (
+                    col >= 4
+                    and board[row][col] == player
+                    and board[row + 1][col - 1] == player
+                    and board[row + 2][col - 2] == player
+                    and board[row + 3][col - 3] == player
+                    and board[row + 4][col - 4] == player
+                ):
                     won_player = player
                     break
             except:
@@ -74,8 +119,15 @@ def check_game_over(player):
             break
     if won_player != 0:
         winner_message = "Black won!" if won_player == 1 else "White won!"
-        canvas.create_text(canvas_width / 2, board_bottom + y_offset / 2, text=winner_message, font=("Helvetica", cell_size), fill="black")
+        canvas.create_text(
+            canvas_width / 2,
+            board_bottom + y_offset / 2,
+            text=winner_message,
+            font=("Helvetica", cell_size),
+            fill="black",
+        )
         game_over = True
+
 
 # Set up the main window
 root = tk.Tk()
@@ -112,8 +164,12 @@ game_over = False
 
 # Draw the grid lines
 for i in range(board_size):
-    canvas.create_line(board_left + i * cell_size, board_top, board_left + i * cell_size, board_bottom) # vertical
-    canvas.create_line(board_left, board_top + i * cell_size, board_right, board_top + i * cell_size) # horizontal
+    canvas.create_line(
+        board_left + i * cell_size, board_top, board_left + i * cell_size, board_bottom
+    )  # vertical
+    canvas.create_line(
+        board_left, board_top + i * cell_size, board_right, board_top + i * cell_size
+    )  # horizontal
 
 # Bind the click event
 canvas.bind("<Button-1>", on_click)
