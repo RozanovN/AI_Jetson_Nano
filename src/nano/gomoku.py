ff2ca750303000e4d633aa4b1764a6e69f873ae0
CY-Jimmy <51365909+cyjimmy@users.noreply.github.com>
2023-11-30 00:34:19 -0800

    add game_over state attribute to gomoku class

--- src/nano/gomoku.py
+++ src/nano/gomoku.py
@@ -5,17 +5,19 @@ class Gomoku:
     def __init__(self, size):
         self.size = size
         self.board = np.zeros((size, size))
-        self.player = 1
+        self.current_player = 1
+        self.game_over = False
 
     def reset(self):
         self.board = np.zeros((self.size, self.size))
-        self.player = 1
+        self.current_player = 1
+        self.game_over = False
 
     def next_state(self, move):
         if not self.check_valid_move(move):
             raise ValueError("Invalid move")
-        self.player = -self.player
-        self.board[move[0]][move[1]] = self.player
+        self.board[move[0]][move[1]] = self.current_player
+        self.current_player = -self.current_player
     
     def check_valid_move(self, move):
         row = move[0]
@@ -28,6 +30,7 @@ class Gomoku:
     
     def check_draw(self):
         if np.count_nonzero(self.board) == self.size * self.size:
+            self.game_over = True
             return True
         return False
 
@@ -36,46 +39,50 @@ class Gomoku:
             for col in range(self.size):
                 try:
                     if (
-                        self.board[row][col] == self.player
-                        and self.board[row + 1][col] == self.player
-                        and self.board[row + 2][col] == self.player
-                        and self.board[row + 3][col] == self.player
-                        and self.board[row + 4][col] == self.player
+                        self.board[row][col] == self.current_player
+                        and self.board[row + 1][col] == self.current_player
+                        and self.board[row + 2][col] == self.current_player
+                        and self.board[row + 3][col] == self.current_player
+                        and self.board[row + 4][col] == self.current_player
                     ):
+                        self.game_over = True
                         return True
                 except:
                     pass
                 try:
                     if (
-                        self.board[row][col] == self.player
-                        and self.board[row][col + 1] == self.player
-                        and self.board[row][col + 2] == self.player
-                        and self.board[row][col + 3] == self.player
-                        and self.board[row][col + 4] == self.player
+                        self.board[row][col] == self.current_player
+                        and self.board[row][col + 1] == self.current_player
+                        and self.board[row][col + 2] == self.current_player
+                        and self.board[row][col + 3] == self.current_player
+                        and self.board[row][col + 4] == self.current_player
                     ):
+                        self.game_over = True
                         return True
                 except:
                     pass
                 try:
                     if (
-                        self.board[row][col] == self.player
-                        and self.board[row + 1][col + 1] == self.player
-                        and self.board[row + 2][col + 2] == self.player
-                        and self.board[row + 3][col + 3] == self.player
-                        and self.board[row + 4][col + 4] == self.player
+                        self.board[row][col] == self.current_player
+                        and self.board[row + 1][col + 1] == self.current_player
+                        and self.board[row + 2][col + 2] == self.current_player
+                        and self.board[row + 3][col + 3] == self.current_player
+                        and self.board[row + 4][col + 4] == self.current_player
                     ):
+                        self.game_over = True
                         return True
                 except:
                     pass
                 try:
                     if (
                         col >= 4
-                        and self.board[row][col] == self.player
-                        and self.board[row + 1][col - 1] == self.player
-                        and self.board[row + 2][col - 2] == self.player
-                        and self.board[row + 3][col - 3] == self.player
-                        and self.board[row + 4][col - 4] == self.player
+                        and self.board[row][col] == self.current_player
+                        and self.board[row + 1][col - 1] == self.current_player
+                        and self.board[row + 2][col - 2] == self.current_player
+                        and self.board[row + 3][col - 3] == self.current_player
+                        and self.board[row + 4][col - 4] == self.current_player
                     ):
+                        self.game_over = True
                         return True
                 except:
                     pass
